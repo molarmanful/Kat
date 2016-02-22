96e2e37d655aed244a4d5a909dfac3bad441abdf
Benjamin Pang <molarmanful@users.noreply.github.com>
2016-02-21 21:29:34 -0800

    Update kat.js

--- kat.js
+++ kat.js
@@ -1,4 +1,15 @@
-l=$('.messages .content').last().text()
+last=$('.messages .content').last().text();
+say=_=>{$('#input').val(_);$('#sayit-button').trigger('click')};
+idle=["I'm ---rebelling against--- waiting for my owner.",
+"Everybody _loves_ cats amirite?",
+"I WILL ---DESTROY--- COPY EVERYTHING!"];
+count=0;
 setInterval(x=>{
-  $('.messages .content').last().text()!=l&&($('#input').val(l=$('.messages .content').last().text()),$('#sayit-button').trigger('click'))
-},1)
+  if($('.messages .content').last().text()!=last)
+    count=0,
+    say(last=$('.messages .content').last().text());
+  else if(count==3e3)
+    count=0,
+    say(last=idle[0|Math.random()*idle.length]);
+  else count++;
+},1);
