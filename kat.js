798f464139f2c61eaa7e089aea9249d283be1ede
Benjamin Pang <molarmanful@users.noreply.github.com>
2016-02-21 20:34:09 -0800

    Update kat.js

--- kat.js
+++ kat.js
@@ -1,1 +1,4 @@
-
+l=$('.messages .content').last().text()
+setInterval(x=>{
+  $('.messages .content').last().text()!=l&&($('#input').val(l=$('.messages .content').last().text()),$('#sayit-button').trigger('click'))
+},1)
